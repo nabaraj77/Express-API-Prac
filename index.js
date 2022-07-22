4855d407b91d991cc6dc04e97b7bcc39e3e34399
Nabaraj <nabaraj.dhl77@gmail.com>
2022-07-22 10:15:47 +0545

    express.static used completed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
+//To show default index.html file while running the server. Code Started--- make aboutUs.html & index.html inside public Folder
+app.use(express.static(`${__dirname}/public`));
+//Code Completed
+
 app.use((req, res) => {
   console.log("Server Created");
+
   res.header("Content-Type", "text-html");
+
   res.statusCode = 200;
   res.end("Hello Nabaraj Dahal");
 });
